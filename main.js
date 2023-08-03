4eaed46ef682e9db5a739aaeca2f2d5c87e0e17b
MehdiLam <52042@etu.he2b.be>
2023-08-03 18:33:56 +0200

    commit

--- main.js
+++ main.js
@@ -8,16 +8,34 @@ const mongoose = require("mongoose");
 
 app.set('view engine','ejs');
 
+
+
+
+//Blacklist
 app.get('/',(req,res)=>{
     res.render('index');
 }
 )
 
+
+
+app.post('/',(req,res)=>{
+    res.render('index');
+}
+)
+
+
+
+//Favori
 app.get('/favori',(req,res)=>{
     res.render('favori');
 }
 )
 
+
+
+
+//Blacklist
 app.get('/blacklist',(req,res)=>{
     res.render('blacklist');
 }
